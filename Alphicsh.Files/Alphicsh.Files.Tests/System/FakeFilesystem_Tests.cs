43d3b365a428b0e4dd5a05c5e5ba8bd8b442ebab
Alice Jankowska <alphish.creature@gmail.com>
2025-04-18 14:35:21 +0200

    ISS-4: Made tests for the fake filesystem

---
+++ Alphicsh.Files/Alphicsh.Files.Tests/System/FakeFilesystem_Tests.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace Alphicsh.Files.System;
+
+public class FakeFilesystem_Tests
+{
+    private CancellationToken CancellationToken => TestContext.Current.CancellationToken;
+
+    [Fact]
+    public async Task FakeFilesystem_ShouldManageFiles()
+    {
+        var filesystem = new FakeFilesystem();
+        var fooPath = new FilePath("/foo.txt");
+        var barPath = new FilePath("/bar.txt");
+
+        filesystem.FileExists(fooPath).ShouldBeFalse();
+        filesystem.FileExists(barPath).ShouldBeFalse();
+
+        await filesystem.WriteFile(fooPath, "Hello!", CancellationToken);
+
+        filesystem.FileExists(fooPath).ShouldBeTrue();
+        (await filesystem.ReadFile(fooPath, CancellationToken)).ShouldBe("Hello!");
+        filesystem.FileExists(barPath).ShouldBeFalse();
+
+        filesystem.MoveFile(fooPath, barPath);
+
+        filesystem.FileExists(fooPath).ShouldBeFalse();
+        filesystem.FileExists(barPath).ShouldBeTrue();
+        (await filesystem.ReadFile(barPath, CancellationToken)).ShouldBe("Hello!");
+
+        filesystem.DeleteFile(barPath);
+
+        filesystem.FileExists(fooPath).ShouldBeFalse();
+        filesystem.FileExists(barPath).ShouldBeFalse();
+    }
+}
