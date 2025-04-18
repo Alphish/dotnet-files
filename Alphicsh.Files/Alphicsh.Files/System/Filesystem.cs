aca8a6ab2313eb4e27fd649ec5412e6243c75cb1
Alice Jankowska <alphish.creature@gmail.com>
2025-04-18 14:34:41 +0200

    ISS-4: Made a basic implementation of the file system wrapper

---
+++ Alphicsh.Files/Alphicsh.Files/System/Filesystem.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Alphicsh.Files.System;
+
+public class Filesystem : IFilesystem
+{
+    public bool FileExists(FilePath path)
+        => File.Exists(path);
+
+    public Task<string> ReadFile(FilePath path, CancellationToken cancellationToken = default)
+        => File.ReadAllTextAsync(path, cancellationToken);
+
+    public Task WriteFile(FilePath path, string content, CancellationToken cancellationToken = default)
+        => File.WriteAllTextAsync(path, content, cancellationToken);
+
+    public void MoveFile(FilePath pathFrom, FilePath pathTo)
+        => File.Move(pathFrom, pathTo, overwrite: true);
+
+    public void DeleteFile(FilePath path)
+        => File.Delete(path);
+}
