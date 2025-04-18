d16e1dbc7e6b37c40265b1700725b9e610155907
Alice Jankowska <alphish.creature@gmail.com>
2025-04-18 14:34:19 +0200

    ISS-4: Fixed the FileRoot namespace problem

--- Alphicsh.Files/Alphicsh.Files/FileRoot.cs
+++ Alphicsh.Files/Alphicsh.Files/FileRoot.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 
+using IOPath = System.IO.Path;
+
 namespace Alphicsh.Files;
 
 public static class FileRoot
 {
     public static string Location { get; } =
-        System.IO.Path.GetPathRoot(typeof(FileRoot).Assembly.Location)?.NormalizePath()
+        IOPath.GetPathRoot(typeof(FileRoot).Assembly.Location)?.NormalizePath()
         ?? throw new NotSupportedException("Could not determine a reference file root.");
 
     public static FilePath Path { get; } = new FilePath(Location);
